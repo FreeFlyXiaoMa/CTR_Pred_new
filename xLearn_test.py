34351d7aa9a524b37e51a09a479aab160df6ab2d
FreeFlyXiaoMa <2429825530@qq.com>
2019-04-14 18:24:36 +0800

    xlearn模型

---
+++ xLearn_test.py
@@ -0,0 +1,23 @@
+import xlearn as xl
+from sklearn.model_selection import train_test_split
+
+#X_train,X_val,y_train
+print(xl.__version__)
+ffm_model=xl.create_ffm()
+#ffm_model.set
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
