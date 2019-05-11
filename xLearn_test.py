e253e05f4312527f5f51f81abf5c0e3da2d9ad54
FreeFlyXiaoMa <2429825530@qq.com>
2019-05-11 19:52:38 +0800

    LSTM RNN模型

--- xLearn_test.py
+++ xLearn_test.py
@@ -1,12 +1,42 @@
-import xlearn as xl
+#import xlearn as xl
 from sklearn.model_selection import train_test_split
 
-#X_train,X_val,y_train
-print(xl.__version__)
-ffm_model=xl.create_ffm()
-#ffm_model.set
+import jieba
+import pandas as pd
+pd.set_option('display.max_columns',1000)
+pd.set_option('display.width',1000)
 
-#33
+pd.set_option('display.max_colwidth',1000)
+#停用词
+stopwords=pd.read_csv('stopwords.txt',index_col=False,quoting=3,
+                      names=['stopword'],encoding='utf-8')
+
+stopwords=stopwords['stopword'].values
+print(stopwords)
+
+#加载语料
+laogong_df=pd.read_csv('beilaogongda.csv',sep=',',encoding='utf-8')
+laopo_df=pd.read_csv('beilaopoda.csv',sep=',',encoding='utf-8')
+erzi_df=pd.read_csv('beierzida.csv',sep=',',encoding='utf-8')
+nver_df=pd.read_csv('beinverda.csv',sep=',',encoding='utf-8')
+
+#删除语料的nan行
+laogong_df.dropna(inplace=True)
+laopo_df.dropna(inplace=True)
+erzi_df.dropna(inplace=True)
+nver_df.dropna(inplace=True)
+
+#转换
+laogong=laogong_df.segment.values.tolist()
+laopo=laopo_df.segment.values.tolist()
+erzi=erzi_df.segment.values.tolist()
+nver=nver_df.segment.values.tolist()
+
+
+
+
+
+
 
 
 
